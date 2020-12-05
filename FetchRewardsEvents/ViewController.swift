97fd975aaabbca8fceeb86b3e6537739521369e9
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-12-05 09:23:51 -0500

    initial commit

---
+++ FetchRewardsEvents/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FetchRewardsEvents
+//
+//  Created by Angela Garrovillas on 12/5/20.
+//  Copyright © 2020 Angela Garrovillas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
