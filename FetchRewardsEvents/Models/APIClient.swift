cfde9b244bc3c4b49c952bc8641dfc69ad1f12e7
AngelaMae_Garrovillas <androiduser1757@gmail.com>
2020-12-08 09:09:14 -0500

    adds fetch events by id in apiCLient

--- FetchRewardsEvents/Models/APIClient.swift
+++ FetchRewardsEvents/Models/APIClient.swift
@@ -33,6 +33,26 @@ struct APIClient {
             }
         }.resume()
     }
+    func getEventBy(id: Int, completion: @escaping (Result<Event,Error>) -> ()) {
+        let urlString = "https://api.seatgeek.com/2/events/\(id)?client_id=\(Secrets.clientId)"
+        guard let url = URL(string: urlString) else {
+            print("id url error")
+        }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+            }
+            guard let data = data else {
+                print("id data error")
+            }
+            do {
+                let event = try JSONDecoder().decode(Event.self, from: data)
+                completion(.success(event))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
     
     func getImageData(urlString: String, completion: @escaping (Result<Data,Error>) ->()) {
         guard let url = URL(string: urlString) else {
